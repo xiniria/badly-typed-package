967a3778b809b193c865c79465cec165d6dbf5cc
Raphaël Dhôte <raphaeld@hokla.com>
2022-11-30 23:29:50 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+export function getIsoDate(arg: number | string | Date): string {
+  if (typeof arg === 'number') {
+    return new Date(arg).toISOString();
+  }
+  if (typeof arg === 'string') {
+    const date = new Date(arg);
+
+    if (Number.isNaN(date.valueOf())) {
+      return 'invalid';
+    } else {
+      return date.toISOString();
+    }
+  }
+  if (arg instanceof Date) {
+    return arg.toISOString();
+  }
+  if (arg === null) {
+    return new Date().toISOString();
+  }
+
+  throw new TypeError('Invalid argument!');
+}
